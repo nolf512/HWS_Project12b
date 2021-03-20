8a15ac21117305f26e0823064ea982d71bd1e4ab
nofprog <k.jin3217@gmail.com>
2021-03-20 23:56:09 +0900

    レイアウト修正

--- HWS_Project10/ViewController.swift
+++ HWS_Project10/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UICollectionViewController {
 
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 10
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {

9e20358219e23620de1345dfec88124c1cc385da
nofprog <k.jin3217@gmail.com>
2021-03-21 23:26:36 +0900

    レイアウト修正

--- HWS_Project10/PersonCell.swift
+++ HWS_Project10/PersonCell.swift
@@ -2,14 +2,12 @@
 //  PersonCell.swift
 //  HWS_Project10
 //
-//  Created by J on 2021/03/19.
+//  Created by J on 2021/03/21.
 //
 
 import UIKit
 
 class PersonCell: UICollectionViewCell {
-    
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var name: UILabel!
-    
 }
--- HWS_Project10/ViewController.swift
+++ HWS_Project10/ViewController.swift
@@ -18,17 +18,17 @@ class ViewController: UICollectionViewController {
         
     }
 
-    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return 2
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
-            fatalError("fatalError")
+            fatalError("fatal error!!")
         }
         return cell
     }
+  
 
 }
 
